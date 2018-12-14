26cef8a7498124853c06a29a3b28e5e5bec3962f
math964g <math964g@edu.eal.dk>
2018-12-14 09:35:08 +0100

    Did stuff

---
+++ BonusApp/Bonuses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public class Bonuses : BonusProvider
+    {
+        public static double TenPercent(double amount)
+        {
+            return amount * 0.10;
+        }
+
+        public static double FlatTwoIfAmountMoreThanFive(double amount)
+        {
+            double returnValue = 0;
+
+            if (amount > 5)
+            {
+                returnValue = 2.0;
+            }
+
+            else
+            {
+                returnValue = 0.0;
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ BonusApp/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public class Order
+    {
+        public BonusProvider Bonus {get; set;}
+        public List<Product> products = new List<Product>();
+
+        public void AddProduct(Product productItem)
+        {
+            products.Add(productItem);
+        }
+
+        public double GetValueOfProducts()
+        {
+            return products.Sum(item => item.Value);
+        }
+
+        public double GetBonus()
+        {
+            return Bonuses.TenPercent(GetValueOfProducts());
+            //double bonusValueAll = 0.0;
+
+            //foreach (Product product in products)
+            //{
+            //    bonusValueAll += Bonuses.TenPercent(product.Value);
+            //}
+
+            //return bonusValueAll;
+        }
+
+        public double GetTotalPrice()
+        {
+            return GetValueOfProducts() - GetBonus();
+        }
+    }
+}
